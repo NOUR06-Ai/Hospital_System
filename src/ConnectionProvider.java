5b4ece373f255223992caf2975f802a42a3a1959
DELL <DELL@NOUR>
2025-11-30 21:41:23 +0200

    Initial project upload

---
+++ src/ConnectionProvider.java
@@ -0,0 +1,23 @@
+ 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+public class ConnectionProvider {
+ 
+   public static Connection getCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/college",
+                    "root",
+                    "061106"
+            );
+            return con;
+        } catch (Exception e) {
+       //System.out.println(e);
+       e.printStackTrace();
+            return null;
+        }
+    }
+}
